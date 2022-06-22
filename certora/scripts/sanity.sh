8310ae600241553656ce4a04d84e6a2b9b7916ee
teryanarmen <teryanarmen@gmail.com>
2022-06-22 14:04:55 -0700

    modify package.json to make scripts run, previously had contract inheritance issues

--- certora/scripts/sanity.sh
+++ certora/scripts/sanity.sh
@@ -1,12 +1,4 @@
 certoraRun \
-    certora/munged/pool-linear/contracts/test/MockLinearPool.sol \
-    --verify MockLinearPool:certora/spec/sanity.spec \
-    --optimistic_loop \
-    --loop_iter 3 \
-    --send_only \
-    --msg "Linear Pool sanity"
-
-certoraRun \
     certora/munged/pool-stable/contracts/test/MockStablePool.sol \
     --verify MockStablePool:certora/spec/sanity.spec \
     --optimistic_loop \
@@ -22,10 +14,3 @@ certoraRun \
     --send_only \
     --msg "Stable Phantom Pool sanity"
 
-certoraRun \
-    certora/munged/pool-weighted/contracts/test/MockWeightCompression.sol \
-    --verify MockWeightCompression:certora/spec/sanity.spec \
-    --optimistic_loop \
-    --loop_iter 3 \
-    --send_only \
-    --msg "Weighted Pool sanity"
