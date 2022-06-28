06b4beec14cb3f0637a505cbace1380530af7f55
Thomas Adams <thomas.harlan.adams@gmail.com>
2022-06-28 15:03:10 -0500

    Added public wrapper functions for internal WordCodec library functions to WordCodecHarness

--- certora/harness/WordCodecHarness.sol
+++ certora/harness/WordCodecHarness.sol
@@ -2,9 +2,86 @@ pragma solidity ^0.7.0;
 
 import "../munged/solidity-utils/contracts/helpers/WordCodec.sol";
 
-// This is the contract that is actually verified; it may contain some helper
-// methods for the spec to access internal state, or may override some of the
-// more complex methods in the original contract.
-contract WordCodecHarness is WordCodec{
-//    using WordCodec for *;
+// This contract calls internal functions from the WordCodec library using 
+// public wrapper functions.
+contract WordCodecHarness {
+    using WordCodec for *;
+
+    // In-place insertion
+
+    function insertUint_wrapper(
+        bytes32 word,
+        uint256 value,
+        uint256 offset,
+        uint256 bitLength
+    ) public pure returns (bytes32) { // HARNESS: internal -> public
+        return insertUint(word, value, offset, bitLength);
+    }
+
+    function insertInt_wrapper(
+        bytes32 word,
+        int256 value,
+        uint256 offset,
+        uint256 bitLength
+    ) public pure returns (bytes32) { // HARNESS: internal -> public
+        return insertInt(word, value, offset, bitLength);
+    }
+
+    // Encoding
+
+    function encodeUint_wrapper(
+        uint256 value,
+        uint256 offset,
+        uint256 bitLength
+    ) public pure returns (bytes32) { // HARNESS: internal -> public
+        return encodeUint(value, offset, bitLength);
+    }
+
+    function encodeInt_wrapper(
+        int256 value,
+        uint256 offset,
+        uint256 bitLength
+    ) public pure returns (bytes32) { // HARNESS: internal -> public
+        return encodeInt(value, offset, bitLength);
+    }
+
+    // Decoding
+
+    function decodeUint_wrapper(
+        bytes32 word,
+        uint256 offset,
+        uint256 bitLength
+    ) public pure returns (uint256) { // HARNESS: internal -> public
+        return decodeUint(word, offset, bitLength);
+    }
+
+    function decodeInt_wrapper(
+        bytes32 word,
+        uint256 offset,
+        uint256 bitLength
+    ) public pure returns (int256) { // HARNESS: internal -> public
+        return decodeInt(word, offset, bitLength);
+    }
+
+    // Special cases
+
+    function decodeBool_wrapper(bytes32 word, uint256 offset) public pure returns (bool) { // HARNESS: internal -> public
+        return decodeBool(word, offset);
+    }
+
+    function insertBits192_wrapper(
+        bytes32 word,
+        bytes32 value,
+        uint256 offset
+    ) public pure returns (bytes32) { // HARNESS: internal -> public
+        return insertBits192(word, value, offset);
+    }
+
+    function insertBool_wrapper(
+        bytes32 word,
+        bool value,
+        uint256 offset
+    ) public pure returns (bytes32) { // HARNESS: internal -> public
+        return insertBool(word, value, offset);
+    }
 }
