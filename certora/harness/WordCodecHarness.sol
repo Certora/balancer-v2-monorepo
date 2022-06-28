0dbfed42ae34b83f1005d39d5eb0e1f6b12a5246
Thomas Adams <thomas.harlan.adams@gmail.com>
2022-06-28 16:58:51 -0500

    Changed function naming scheme to remove _wrapper suffixes

--- certora/harness/WordCodecHarness.sol
+++ certora/harness/WordCodecHarness.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.7.0;
 
-import "../munged/solidity-utils/contracts/helpers/WordCodec.sol";
+import {WordCodec} from "../munged/solidity-utils/contracts/helpers/WordCodec.sol";
 
 // This contract calls internal functions from the WordCodec library using 
 // public wrapper functions.
@@ -9,79 +9,79 @@ contract WordCodecHarness {
 
     // In-place insertion
 
-    function insertUint_wrapper(
+    function insertUint(
         bytes32 word,
         uint256 value,
         uint256 offset,
         uint256 bitLength
     ) public pure returns (bytes32) { // HARNESS: internal -> public
-        return insertUint(word, value, offset, bitLength);
+        return WordCodec.insertUint(word, value, offset, bitLength);
     }
 
-    function insertInt_wrapper(
+    function insertInt(
         bytes32 word,
         int256 value,
         uint256 offset,
         uint256 bitLength
     ) public pure returns (bytes32) { // HARNESS: internal -> public
-        return insertInt(word, value, offset, bitLength);
+        return WordCodec.insertInt(word, value, offset, bitLength);
     }
 
     // Encoding
 
-    function encodeUint_wrapper(
+    function encodeUint(
         uint256 value,
         uint256 offset,
         uint256 bitLength
     ) public pure returns (bytes32) { // HARNESS: internal -> public
-        return encodeUint(value, offset, bitLength);
+        return WordCodec.encodeUint(value, offset, bitLength);
     }
 
-    function encodeInt_wrapper(
+    function encodeInt(
         int256 value,
         uint256 offset,
         uint256 bitLength
     ) public pure returns (bytes32) { // HARNESS: internal -> public
-        return encodeInt(value, offset, bitLength);
+        return WordCodec.encodeInt(value, offset, bitLength);
     }
 
     // Decoding
 
-    function decodeUint_wrapper(
+    function decodeUint(
         bytes32 word,
         uint256 offset,
         uint256 bitLength
     ) public pure returns (uint256) { // HARNESS: internal -> public
-        return decodeUint(word, offset, bitLength);
+        return WordCodec.decodeUint(word, offset, bitLength);
     }
 
-    function decodeInt_wrapper(
+    function decodeInt(
         bytes32 word,
         uint256 offset,
         uint256 bitLength
     ) public pure returns (int256) { // HARNESS: internal -> public
-        return decodeInt(word, offset, bitLength);
+        return WordCodec.decodeInt(word, offset, bitLength);
     }
 
     // Special cases
 
-    function decodeBool_wrapper(bytes32 word, uint256 offset) public pure returns (bool) { // HARNESS: internal -> public
-        return decodeBool(word, offset);
+    function decodeBool(bytes32 word, uint256 offset) public pure returns (bool) { // HARNESS: internal -> public
+        return WordCodec.decodeBool(word, offset);
     }
 
-    function insertBits192_wrapper(
+    function insertBits192(
         bytes32 word,
         bytes32 value,
         uint256 offset
     ) public pure returns (bytes32) { // HARNESS: internal -> public
-        return insertBits192(word, value, offset);
+        return WordCodec.insertBits192(word, value, offset);
     }
 
-    function insertBool_wrapper(
+    function insertBool(
         bytes32 word,
         bool value,
         uint256 offset
     ) public pure returns (bytes32) { // HARNESS: internal -> public
-        return insertBool(word, value, offset);
+        return WordCodec.insertBool(word, value, offset);
     }
 }
