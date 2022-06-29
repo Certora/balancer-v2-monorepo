9aeb68826e64f0827a062a16b625bba6e8233b4b
Thomas Adams <thomas.harlan.adams@gmail.com>
2022-06-29 12:41:35 -0500

    Added WordCodec run script and spec file

---
+++ certora/scripts/verifyWordCodec.sh
@@ -0,0 +1,12 @@
+make -C certora munged
+
+certoraRun \
+    certora/harness/WordCodecHarness.sol \
+    --verify WordCodecHarness:certora/spec/WordCodec.spec \
+    --solc solc7.1 \
+    --optimistic_loop \
+    --loop_iter 3 \
+    --send_only \
+    --cloud \
+    --settings -useBitVectorTheory \
+    --msg "WordCodec verification all rules"
