aee8d2b5a3d0190ae08f83cff8bb87167f9fc88b
Tadeas Kucera <tadeas@certora.com>
2023-03-27 13:39:07 +0200

    Delete AuthentucationHarness as it is not used

--- certora/harness/SingletonAuthenticationHarness.sol
+++
@@ -1,6 +0,0 @@
-import "../munged/solidity-utils/contracts/helpers/SingletonAuthentication.sol";
-
-contract SingletonAuthenticationHarness is SingletonAuthentication {
-    constructor(IVault vault) SingletonAuthentication(vault) { }
-
-}
