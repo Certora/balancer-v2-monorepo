401d588bbc883415f125e962c9d461f1f839faf0
Aleksander Kryukov <firealexkryukov@gmail.com>
2023-03-27 12:13:04 +0100

    fixing import

--- certora/munged/vault/contracts/authorizer/TimelockAuthorizerManagement.sol
+++ certora/munged/vault/contracts/authorizer/TimelockAuthorizerManagement.sol
@@ -20,7 +20,8 @@ import "@balancer-labs/v2-interfaces/contracts/vault/IVault.sol";
 import "@balancer-labs/v2-interfaces/contracts/vault/IAuthorizer.sol";
 import "@balancer-labs/v2-interfaces/contracts/vault/ITimelockAuthorizer.sol";
 
-import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Address.sol";
+// import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/Address.sol";
+import "../../../../harness/Address.sol";   // HARNESS: Using harnessed Address.sol (harnessing low-level code) instead of OZ(above)
 import "@balancer-labs/v2-solidity-utils/contracts/openzeppelin/ReentrancyGuard.sol";
 import "./TimelockExecutionHelper.sol";
 
